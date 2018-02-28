54bb78a2a27834030e5b909f47876f67cc4dc4de
Hsuanchen Wan <WalterHWan@gmail.com>
2018-02-28 15:20:08 -0800

    finished associations exercise

--- associations_exercise/app/models/course.rb
+++ associations_exercise/app/models/course.rb
@@ -1,2 +1,55 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ApplicationRecord
+    has_many(
+      :enrollments,
+      class_name: 'Enrollment',
+      foreign_key: :course_id,
+      primary_key: :id
+    )
+
+    has_many(
+      :enrolled_students,
+      through: :enrollments,
+      source: :user
+    )
+
+    # has_many(
+    #   :prerequisite,
+    #   class_name: 'Course',
+    #   primary_key: :prereq_id,
+    #   foreign_key: :id
+    # )
+
+    belongs_to(
+      :prerequisite,
+      class_name: 'Course',
+      primary_key: :id,
+      foreign_key: :prereq_id,
+    )
+
+    #
+    # has_many(
+    #   :instructor,
+    #   class_name: 'User',
+    #   primary_key: :instructor_id,
+    #   foreign_key: :id
+    # )
+
+    belongs_to(
+      :instructor,
+      class_name: 'User',
+      primary_key: :id,
+      foreign_key: :instructor_id
+    )
 end
